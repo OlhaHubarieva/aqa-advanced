d15135977e9a75e94e1b0aa8bcbf6cdac04796b8
Your Name <you@example.com>
2024-07-12 22:13:53 +0300

    node_modules, jsBasics

---
+++ jsBasics/task1.js
@@ -0,0 +1,11 @@
+let name = 'Olha';
+let age = 42;
+let isAdult = true;
+let work = null;
+let position;
+
+console.log(name);
+console.log(age);
+console.log(isAdult);
+console.log(work);
+console.log(position);
