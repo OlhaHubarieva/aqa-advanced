ffbcb44f929710eb7b72136473bff89b243a056b
Olgagub <hubba.oljuna@gmail.com>
2024-08-11 22:44:51 +0300

    objects_practice homework added

---
+++ objects_practice/task4.js
@@ -0,0 +1,20 @@
+/*Завдання 4
+Створіть об'єкт person з такими властивостями:
+firstName: рядок, представляє ім'я особи.
+lastName: рядок, представляє прізвище особи.
+age: число, представляє вік особи.
+Додайте до об'єкта person властивість email, яка буде містити електронну пошту особи.
+Видаліть властивість age з об'єкта person.
+Виведіть оновлений об'єкт person в консоль.*/
+
+const person = {
+  firstName: "Alice",
+  lastName: "Doe",
+  age: 10,
+};
+
+person.email = "alice@gmail.com";
+
+delete person.age;
+
+console.log(person);
